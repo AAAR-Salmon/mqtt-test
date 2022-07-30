304c56786b51258bebd39e3a6e39e7a0110cc6a2
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-07-30 19:40:25 +0900

    add: mqtt client

---
+++ client/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var options *mqtt.ClientOptions = mqtt.NewClientOptions().AddBroker("tcp://broker.emqx.io:1883")
+var msgCh = make(chan mqtt.Message)
+
+func onReceive(client mqtt.Client, msg mqtt.Message) {
+	msgCh <- msg
+}
+
+func publish(client mqtt.Client) {
+	if token := client.Publish("mqtt-test", 0, false, "a boring message"); token.Wait() && token.Error() != nil {
+		log.Fatalln(token.Error())
+	}
+}
+
+func main() {
+	client := mqtt.NewClient(options)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalln(token.Error())
+	}
+
+	if token := client.Subscribe("mqtt-test", 0, onReceive); token.Wait() && token.Error() != nil {
+		log.Fatalln(token.Error())
+	}
+
+	go func() {
+		for {
+			publish(client)
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+	}()
+
+	for {
+		m := <-msgCh
+		fmt.Printf("topic: %v, payload: %v\n", m.Topic(), string(m.Payload()))
+	}
+}
